c98b9c1e428deeb6085f1886687369ac469f6d11
mustard123 <silasdavidweber@gmail.com>
2019-03-11 10:35:40 +0100

    init project structure

---
+++ RecommenderSystem/src/main/java/uzh/ch/ase19/App.java
@@ -0,0 +1,13 @@
+package uzh.ch.ase19;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
