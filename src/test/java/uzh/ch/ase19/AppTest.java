7cfad4c691feb8199a7efa030ebda54a556060ee
stewue <stefan.wuersten@uzh.ch>
2019-03-21 11:45:31 +0100

    Fix test

--- src/test/java/uzh/ch/ase19/AppTest.java
+++ src/test/java/uzh/ch/ase19/AppTest.java
@@ -1,7 +1,7 @@
 package uzh.ch.ase19;
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
