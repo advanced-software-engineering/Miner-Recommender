dd3c59b250edbe071b8524827fb096981e41ecd6
stewue <stefan@wuersten.ch>
2019-04-28 12:12:38 +0200

    Add context processor test case

---
+++ src/test/java/ch/uzh/ifi/seal/ase19/miner/ContextProcessorTest.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.seal.ase19.miner;
+
+import cc.kave.commons.model.events.completionevents.Context;
+import cc.kave.commons.model.typeshapes.ITypeShape;
+import ch.uzh.ifi.seal.ase19.core.PersistenceManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class ContextProcessorTest {
+
+    private PersistenceManager pm;
+    private ContextProcessor sut;
+
+    @BeforeEach
+    void before() {
+        pm = mock(PersistenceManager.class);
+        sut = new ContextProcessor(pm);
+    }
+
+    @Test
+    void run() {
+        Context c = mock(Context.class);
+        ITypeShape ts = mock(ITypeShape.class);
+        when(ts.getMethodHierarchies()).thenReturn(null);
+        when(ts.getFields()).thenReturn(null);
+        when(c.getTypeShape()).thenReturn(ts);
+
+        sut.run(c);
+
+        verify(pm, times(0)).save(any());
+    }
+}
