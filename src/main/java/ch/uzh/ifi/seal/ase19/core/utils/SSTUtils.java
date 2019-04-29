7100b03dc3b70974153d12883f9e2d0f583da47d
stewue <stefan@wuersten.ch>
2019-04-29 08:14:57 +0200

    Fix Problem with generics

--- src/main/java/ch/uzh/ifi/seal/ase19/core/utils/SSTUtils.java
+++ src/main/java/ch/uzh/ifi/seal/ase19/core/utils/SSTUtils.java
@@ -3,9 +3,13 @@ package ch.uzh.ifi.seal.ase19.core.utils;
 public class SSTUtils {
     public static String getFullyQualifiedNameWithoutGenerics(String in) {
         if (in.contains("`")) {
-            return in.substring(0, in.indexOf("`"));
-        } else {
-            return in;
+            in = in.substring(0, in.indexOf("`"));
         }
+
+        if (in.contains("?")) {
+            in = in.substring(0, in.indexOf("?"));
+        }
+
+        return in;
     }
 }
--- src/test/java/ch/uzh/ifi/seal/ase19/core/utils/SSTUtilsTest.java
+++ src/test/java/ch/uzh/ifi/seal/ase19/core/utils/SSTUtilsTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 class SSTUtilsTest {
 
     @Test
-    void getFullyQualifiedNameWithoutGenerics() {
+    void getFullyQualifiedNameWithoutGenerics1() {
         String fullyQualifiedName = "com.Other`?`";
 
         Assertions.assertEquals("com.Other", SSTUtils.getFullyQualifiedNameWithoutGenerics(fullyQualifiedName));
+    }
+
+    @Test
+    void getFullyQualifiedNameWithoutGenerics2() {
+        String fullyQualifiedName = "?";
+
+        Assertions.assertEquals("", SSTUtils.getFullyQualifiedNameWithoutGenerics(fullyQualifiedName));
     }
 }
